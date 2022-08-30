43ebe16a5214b7cadc758498e0af148c81632a0e
VedangSardessai <vms112000@gmail.com>
2022-08-31 00:53:11 +0530

    feature:Navigate to news details

--- lib/models/news_models/news_card.dart
+++ lib/models/news_models/news_card.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:for_x/models/news_models/article_model.dart';
+import 'package:for_x/models/news_models/news_details.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-  Widget customNewsCard(Article article) {
-    return Container(
+Widget customNewsCard(Article article, BuildContext context) {
+  return InkWell(
+    onTap: () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => NewsDetails(article)));
+    },
+    child: Container(
       margin: const EdgeInsets.all(12),
       padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 5),
           color: Colors.white,
           borderRadius: BorderRadius.circular(18),
           boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 3)]),
@@ -15,27 +22,39 @@ import 'package:google_fonts/google_fonts.dart';
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
+            height: 250,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image:
-                    DecorationImage(image: NetworkImage(article.urlToImage!))),
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: NetworkImage(article.urlToImage!),
+              ),
+            ),
           ),
           const SizedBox(height: 8),
           Container(
+            padding: EdgeInsets.all(5),
             decoration: BoxDecoration(
               color: Colors.red,
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(5),
             ),
-            child: Text(article.source!.name.toString()),
+            child: Text(
+              article.source!.name.toString(),
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          const SizedBox(height: 8),
-          Text(article.title.toString() , style: GoogleFonts.poppins(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-
-          ),)
+          const SizedBox(height: 5),
+          Text(
+            article.title.toString(),
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          )
         ],
       ),
-    );
-  }
-
+    ),
+  );
+}
