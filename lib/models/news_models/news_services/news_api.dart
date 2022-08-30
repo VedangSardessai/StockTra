b9bc71e0fdece9d46964eba20988358e60648008
VedangSardessai <vms112000@gmail.com>
2022-08-31 00:53:59 +0530

    feature: Changed Wall Street Journal Articles to -> Nifty based articles

--- lib/models/news_models/news_services/news_api.dart
+++ lib/models/news_models/news_services/news_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart';
 
 class APIServices {
   final endPointURL =
-      "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=97dc31d3a1d24e0eab8777c218537e41";
+      "https://newsapi.org/v2/everything?q=nifty50&apiKey=97dc31d3a1d24e0eab8777c218537e41";
 
   Future<List<Article>> getArticle() async {
     Response response = await get(Uri.parse(endPointURL));
